7611e063bd390cfcfa085811ddfd9f87c4dbf0f2
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2024-10-22 19:57:24 +0400

    Added simple command parser

--- kernel/shell.c
+++ kernel/shell.c
@@ -3,6 +3,9 @@
 #include "lib/malloc.h"
 #include "lib/stdio.h"
 
+#define MAX_TOKENS 100
+#define MAX_TOKEN_LENGTH 256
+
 const char *prompt() {
     static char input[512];
     set_color(COLOR_LYELLOW, COLOR_BLACK);
@@ -13,6 +16,41 @@ const char *prompt() {
     return input;
 }
 
+void spltoks(const char *input, char tokens[MAX_TOKENS][MAX_TOKEN_LENGTH],
+             int *token_count) {
+    bool in_quotes = false;
+    int token_index = 0;
+    int char_index = 0;
+
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (input[i] == '\"')
+            in_quotes = !in_quotes;
+        else if (input[i] == ' ' && !in_quotes) {
+            if (char_index > 0) {
+                tokens[token_index][char_index] = '\0';
+                token_index++;
+                char_index = 0;
+            }
+        } else
+            tokens[token_index][char_index++] = input[i];
+    }
+
+    if (char_index > 0) {
+        tokens[token_index][char_index] = '\0';
+        token_index++;
+    }
+
+    *token_count = token_index;
+}
+
 void shell() {
-    while (true) { printf("Input: %s\n", prompt()); }
+    while (true) {
+        char tokens[MAX_TOKENS][MAX_TOKEN_LENGTH];
+        int token_count;
+
+        const char *input = prompt();
+        spltoks(input, tokens, &token_count);
+
+        printf("Executing command %s\n", tokens[0]);
+    }
 }
