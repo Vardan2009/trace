eab86fc8cee7e1f97886218db1bd98765fa44438
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2024-10-22 21:47:12 +0400

    Added `memcmp`

--- include/lib/string.h
+++ include/lib/string.h
@@ -10,5 +10,6 @@ int strcmp(const char *s1, const char *s2);
 char *strcpy(char *dest, const char *src);
 size_t strlen(const char *s);
 char *strcat(char *dest, const char *src);
+int memcmp(const void *ptr1, const void *ptr2, size_t num);
 
 #endif  // STRING_H
