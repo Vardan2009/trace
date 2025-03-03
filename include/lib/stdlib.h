d4c270e01a7c7d430ff01e020e347ea3cc418a97
Vardan2009 <vpetrosyan100@gmail.com>
2025-03-03 17:00:32 +0400

    Added functions `itoa` and `isdigit`

--- include/lib/stdlib.h
+++ include/lib/stdlib.h
@@ -1,5 +1,7 @@
 #ifndef STDLIB_H
 
 char toupper(char);
+int isdigit(char);
+void itoa(int, char *);
 
 #endif // STDLIB_H
--- kernel/lib/stdlib.c
+++ kernel/lib/stdlib.c
@@ -4,3 +4,29 @@ char toupper(char c) {
     if (c >= 'a' && c <= 'z') return c - ('a' - 'A');
     return c;
 }
+
+int isdigit(char c) {
+    return (c >= '0') && (c <= '9');
+}
+
+void itoa(int value, char *buffer) {
+    char temp[12];
+    int i = 0, is_negative = 0;
+
+    if (value < 0) {
+        is_negative = 1;
+        value = -value;
+    }
+
+    do {
+        temp[i++] = '0' + (value % 10);
+        value /= 10;
+    } while (value > 0);
+
+    if (is_negative) temp[i++] = '-';
+
+    int a = 0;
+    for (int j = i - 1; j >= 0; --j)
+        buffer[a++] = temp[j];
+    buffer[a] = '\0';
+}
